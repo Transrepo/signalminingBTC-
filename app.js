5aa7e4ccda277924514b2378527fec1da579f6b9
piusrepo <piuspolocha8@gmail.com>
2024-11-12 11:46:42 +0100

    grace

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const mongoose  = require('mongoose');
+const session = require('express-session');
+const flash = require('connect-flash');
+const fileUpload = require('express-fileupload')
+const cookieParser = require('cookie-parser');
+const methodOverride = require('method-override');
+const { requireAuth, checkUser } = require('./server/authMiddleware/authMiddleware');
+
+
+const app = express();
+const PORT = 5000 || process.env.PORT;
+
+//middlewares
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+// use file-upload // to import pictures to our website but we required it first above
+app.use(fileUpload());
+app.use(methodOverride('_method'));
+
+// flash message
+app.use(session({
+    secret: 'CookingBlogSecretSession',
+    saveUninitialized: true,
+    resave: true
+  }));
+  app.use(flash());
+
+// set view engine
+app.set('view engine', 'ejs');
+
+//DB config
+const db ='mongodb+srv://pius1:pius123@webdevelopment.xav1dsx.mongodb.net/worlex';
+// connect to mongodb
+mongoose.connect(db)
+.then(()=>{
+    console.log('MongoDB Connected')
+})
+.catch(err =>{console.log(err)})
+
+
+
+
+app.get('*', checkUser);
+app.use('/', require('./server/Route/indexRoute'));
+app.use('/',requireAuth, require('./server/Route/userRoute'));
+app.use('/', requireAuth, require('./server/Route/adminRoute'));
+
+app.listen(PORT, console.log(`Server running on  ${PORT}`));
+
+
+ 
+
+
+
---
+++ server/Model/User.js
@@ -0,0 +1,110 @@
+const mongoose = require('mongoose');
+const  validator  = require('validator');
+// const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    fullname:{
+        type: String,
+    },
+    tel:{
+        type: String,
+    },
+    email:{
+        type: String,
+        unique: true,
+        lowercase: true,
+        required: validator.isEmail['Please enter an email']
+        // required:  [isEmail, 'Please enter an email']
+    },
+    country:{
+        type: String
+    },
+    currency:{
+        type: String
+    },
+
+    password:{
+        type: String,
+    },
+    image:{
+        type: String,
+        default: "/profile/default.png"
+    },
+    accNo:{
+        type: String,
+        default: "Loading"
+    }, 
+    balance:{
+        type: Number,
+        default: 0
+    },
+
+    profit:{
+        type: Number,
+        default: 0
+    },
+    totalwithdraw:{
+        type: Number,
+        default: 0
+    },
+    signals: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'signal'
+    },
+    copytrades: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'copytrade'
+    },
+    livetrades:{
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'livetrade'
+    },
+    upgrades: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'upgrade'
+    },
+    Loan: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'loan'
+    },
+    verified:{
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'verify'
+    },
+   
+    deposits:{
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'deposit'
+    },
+
+    widthdraws:{
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'widthdraw'
+    },
+    Tickets:{
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'ticket'
+    },
+    role:{
+        type: Number,
+        default: 0
+    }
+},{timestamps: true})
+
+// static method to login user
+userSchema.statics.login = async function(email, password) {
+    const user = await this.findOne({ email });
+    if (user) {
+      const auth = await (password, user.password);
+      if (auth) {
+        return user;
+      }
+      throw Error('incorrect password');
+    }
+    throw Error('incorrect email');
+  };
+  
+
+const User = mongoose.model('user', userSchema)
+
+module.exports = User;
